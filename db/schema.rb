c1ea059f9c9f093c794735476d95c556a19eede8
vita <dragic.hsieh@gmail.com>
2018-09-21 01:22:53 +0800

    Done add column image to product table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_20_151102) do
+ActiveRecord::Schema.define(version: 2018_09_20_172124) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_09_20_151102) do
     t.integer "special_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
