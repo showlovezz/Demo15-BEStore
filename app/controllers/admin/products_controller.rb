30415553bf88324f7be7fea66c1e224c286518ef
vita <dragic.hsieh@gmail.com>
2018-09-21 00:06:50 +0800

    Done add product index action and view for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,5 +2,9 @@ class Admin::ProductsController < ApplicationController
 	# 進入後台首頁需註冊及身份為管理員
 	before_action :authenticate_user!
 	before_action :authenticate_admin
+
+	def index
+		@products = Product.all
+	end
 	
 end
