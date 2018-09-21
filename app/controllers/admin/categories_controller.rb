64d4ff0b800510c2a4f73a15201e9d1385626ed7
vita <dragic.hsieh@gmail.com>
2018-09-21 22:49:13 +0800

    Done add form on index page for creating new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,5 +4,24 @@ class Admin::CategoriesController < ApplicationController
 
   def index
 	  @categories = Category.all
+	  @category = Category.new
 	end
+
+	def create
+	  @category = Category.new(category_params)
+	  if @category.save
+	    flash[:notice] = "產品分類已成功建立!"
+	    redirect_to admin_categories_path
+	  else
+	    @categories = Category.all
+	    render :index
+	  end
+	end
+
+	private
+
+	def category_params
+	  params.require(:category).permit(:name)
+	end
+	
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+	# 驗證產品必填
+	validates :name, presence: true
+
 	# 一個分類底下有很多產品
 	has_many :products, dependent: :destroy
 end
