6e47bf6048ac93bbe7df0ddae579a14e274c356d
vita <dragic.hsieh@gmail.com>
2018-09-21 21:04:58 +0800

    Done add category select option in product shared form

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -48,7 +48,7 @@ class Admin::ProductsController < ApplicationController
 	private
 
 	def product_params
-		params.require(:product).permit(:name, :description, :tag, :basic_price, :special_price, :image)
+		params.require(:product).permit(:name, :description, :tag, :basic_price, :special_price, :image, :category_id)
 	end
 
 	def find_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
 	# 驗證產品必填
-	validates :name, :tag, :description, :basic_price, :special_price , presence: true
+	validates :name, :tag, :description, :basic_price, :special_price, :image, :category_id, presence: true
 
 	# 圖片上傳器
 	mount_uploader :image, PhotoUploader
