4b0ae41bcde4eaff5d4665b8a6d3812d5681ed4e
vita <dragic.hsieh@gmail.com>
2018-09-26 21:31:31 +0800

    Done add categories tab on header and footer for public users

--- app/controllers/bestores_controller.rb
+++ app/controllers/bestores_controller.rb
@@ -3,6 +3,9 @@ class BestoresController < ApplicationController
 	def index
 		# 首頁依創立時間秀出 8 個產品
 		@products = Product.order("created_at DESC").limit(8)
+
+		# 秀出全部的分類
+		@categories = Category.all
 	end
 	
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,9 @@ class ProductsController < ApplicationController
 	def show
 		# 秀出特定產品的 Id
 	  @product = Product.find(params[:id])
+
+	  # 秀出全部的分類
+		@categories = Category.all
 	end
 
 end
