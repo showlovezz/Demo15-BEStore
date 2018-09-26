8f990b34d8a7e8de507d6162d605d246eb378cb8
vita <dragic.hsieh@gmail.com>
2018-09-27 00:21:08 +0800

    Done user on index page can select product by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+	def show
+		# 配合導覽列需秀出全部的產品分類
+		@categories = Category.all
+		
+		# 被點擊的分類——特定一筆分類資料
+		@category = Category.find(params[:id])
+		
+		# 和該分類關聯的產品資料
+		@products = @category.products.all
+	end
 end
